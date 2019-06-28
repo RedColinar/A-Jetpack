54d975d07e9aee652508a94436def544cb49915c
RedColinar <1126525692@qq.com>
2019-06-29 00:27:46 +0800

    navigation to fragment

--- app/src/main/java/com/opq/a/jetpack/MainActivity.kt
+++ app/src/main/java/com/opq/a/jetpack/MainActivity.kt
@@ -2,11 +2,20 @@ package com.opq.a.jetpack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.my_nav_host_fragment)
+
     }
+
+    override fun onSupportNavigateUp() = navController.navigateUp()
 }
---
+++ app/src/main/java/com/opq/a/jetpack/SplashFragment.kt
@@ -0,0 +1,75 @@
+package com.opq.a.jetpack
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.fragment_splash.*
+
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [SplashFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [SplashFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class SplashFragment : Fragment() {
+    private var param1: String? = null
+    private var param2: String? = null
+    private lateinit var naviController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_splash, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        naviController = findNavController()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bt_to_login.setOnClickListener {
+            // naviController.navigate(R.id.loginFragment)
+            naviController.navigate(R.id.action_splashFragment_to_loginFragment)
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            SplashFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
