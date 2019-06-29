cff4ddae5f200cbe63944cd35ba7263ef2f37704
panqiang <1126525692@qq.com>
2019-06-29 09:09:20 +0800

    code style

--- app/src/main/java/com/opq/a/jetpack/LoginFragment.kt
+++ app/src/main/java/com/opq/a/jetpack/LoginFragment.kt
@@ -1,30 +1,15 @@
 package com.opq.a.jetpack
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [LoginFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [LoginFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class LoginFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -40,16 +25,7 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-    }
-
-    override fun onDetach() {
-        super.onDetach()
     }
 
     companion object {
--- app/src/main/java/com/opq/a/jetpack/SplashFragment.kt
+++ app/src/main/java/com/opq/a/jetpack/SplashFragment.kt
@@ -1,7 +1,6 @@
 package com.opq.a.jetpack
 
 import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -9,25 +8,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.*
 import kotlinx.android.synthetic.main.fragment_splash.*
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [SplashFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [SplashFragment.newInstance] factory method to
- * create an instance of this fragment.
- *
- */
 class SplashFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
-    private lateinit var naviController: NavController
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,25 +30,21 @@ class SplashFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
-        naviController = findNavController()
+        navController = findNavController()
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         bt_to_login.setOnClickListener {
-            // naviController.navigate(R.id.loginFragment)
-            naviController.navigate(R.id.action_splashFragment_to_loginFragment)
+            // action 或者 destination id
+            // navController.navigate(R.id.loginFragment)
+            navController.navigate(R.id.action_splashFragment_to_loginFragment)
         }
-    }
-
-    override fun onDetach() {
-        super.onDetach()
     }
 
     companion object {
