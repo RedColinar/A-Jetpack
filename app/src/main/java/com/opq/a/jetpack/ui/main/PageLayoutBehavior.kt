59fb91f7922772915056d31ddecadc5a92c84897
RedColinar <1126525692@qq.com>
2019-09-02 22:56:35 +0800

    beavior

---
+++ app/src/main/java/com/opq/a/jetpack/ui/main/PageLayoutBehavior.kt
@@ -0,0 +1,11 @@
+package com.opq.a.jetpack.ui.main
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import java.util.jar.Attributes
+
+class PageLayoutBehavior(context: Context, attr: AttributeSet) : CoordinatorLayout.Behavior<PageLayout>(context, attr) {
+
+
+}
