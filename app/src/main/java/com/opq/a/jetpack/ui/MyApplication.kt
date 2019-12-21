ecf8aaccac314ae232ce248fef71732c0e654422
panqiang <1126525692@qq.com>
2019-12-21 17:55:46 +0800

    listen chrome DevTool

--- app/src/main/java/com/opq/a/jetpack/ui/MyApplication.kt
+++ app/src/main/java/com/opq/a/jetpack/ui/MyApplication.kt
@@ -17,6 +17,6 @@ class MyApplication : Application() {
         super.onCreate()
         app = this
         JetpackDatabase.instance()
-        Stetho.initializeWithDefaults(this)
+        // Stetho.initializeWithDefaults(this)
     }
 }
--- app/src/main/java/com/opq/a/jetpack/ui/SplashFragment.kt
+++ app/src/main/java/com/opq/a/jetpack/ui/SplashFragment.kt
@@ -1,17 +1,28 @@
 package com.opq.a.jetpack.ui
 
 import android.content.Context
+import android.net.LocalServerSocket
+import android.net.LocalSocket
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
+import com.facebook.stetho.common.LogUtil
 import com.opq.a.jetpack.R
 import com.opq.a.jetpack.db.JetpackDatabase
 import com.opq.a.jetpack.db.UserEntity
 import kotlinx.android.synthetic.main.fragment_splash.*
+
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.io.InterruptedIOException
+import java.net.SocketException
+
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -59,6 +70,8 @@ class SplashFragment : Fragment() {
         }
 
         JetpackDatabase.instance().userEntityDao().insertAll(UserEntity("1", "opq"))
+
+        listenDevTool()
     }
 
     companion object {
@@ -70,5 +83,48 @@ class SplashFragment : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+
+    private fun listenDevTool() {
+        // chrome://inspect/#devices
+        val thread = Thread {
+            val address = "opq_devtools_remote"
+            val serverSocket = LocalServerSocket(address)
+            LogUtil.i("Listening on @$address")
+
+            while (!Thread.interrupted()) {
+                try {
+                    val socket: LocalSocket = serverSocket.accept()
+                    // Start worker thread
+//                    val t: Thread = LocalSocketServer.WorkerThread(socket, mSocketHandler)
+//                    t.name = LocalSocketServer.WORKER_THREAD_NAME_PREFIX +
+//                            "-" + mFriendlyName +
+//                            "-" + mThreadId.incrementAndGet()
+//                    t.isDaemon = true
+//                    t.start()
+                    val s = BufferedReader(InputStreamReader(socket.inputStream, "UTF-8"))
+                    s.forEachLine {
+                        Log.d(address, it)
+                    }
+                } catch (se: SocketException) { // ignore exception if interrupting the thread
+                    if (Thread.interrupted()) {
+                        break
+                    }
+                    Log.d(address, "I/O error")
+                } catch (ex: InterruptedIOException) {
+                    break
+                } catch (e: IOException) {
+                    LogUtil.w(
+                        address,
+                        "I/O error initialising connection thread"
+                    )
+                    break
+                }
+            }
+
+            Log.d(address, "Server shutdown on @$address")
+        }
+        thread.isDaemon = true
+        thread.start()
     }
 }
